dbcdbc5a1866b091dd55078ec6f3a23351bfbfd9
N/A <N/A>
2019-09-18 06:08:46 +0300

    ! lib.rs readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 //! Thin wrapper around the Windows IO completion port.
-//! See [`I/O Completion Ports`](https://docs.microsoft.com/en-us/windows/win32/fileio/i-o-completion-ports).
+//!
+//! See [`I/O Completion Ports`](https://docs.microsoft.com/en-us/windows/win32/fileio/i-o-completion-ports) on MSDN for the description of the concept.
+//!
+//! Run
+//! ```
+//! cargo --doc
+//! ```
+//! for documentation.
+//!
+//! Uses [`winapi`](https://docs.rs/winapi/0.3.8/winapi/).
 
 mod iocp;
 
